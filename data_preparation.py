82527a24a159ef752b33adac9a9e7c0853a19579
Davide-Console <davideconsole99@gmail.com>
2022-12-17 20:42:54 +0100

    data preparation

---
+++ data_preparation.py
@@ -0,0 +1,31 @@
+from data_utils import *
+import imageio
+from skimage.transform import resize
+import matplotlib.image as mpimg
+from PIL import Image
+import PIL
+
+def main():
+    path = 'Data/24759123/20010101/MR3'
+
+    # Convert uint16 .dcm images to uint8 .png images
+    dcm_files = [f for f in os.listdir(path) if is_dicom(path+'/'+f)]
+    for i, filename in enumerate(dcm_files):
+        print(f'Converting {filename} ({i+1}/{len(dcm_files)})')
+        dcm2png(path+'/'+filename)
+
+    # Set all images to 256x256 and delete gantry
+    png_files = [f for f in os.listdir(path) if f.endswith('.png')]
+    for i, filename in enumerate(png_files):
+        print(f'Processing {filename} ({i+1}/{len(png_files)})')
+
+        img = Image.open(path+'/'+filename)
+        img = np.array(img)
+        img = resize(img, (256, 256))
+        img = gantry_removal(img)
+        img = Image.fromarray(img).convert('L')
+        img.save(path+'/'+filename)
+
+
+if __name__ == '__main__':
+    main()
---
+++ data_utils.py
@@ -0,0 +1,44 @@
+import pydicom as pd
+import os
+import imageio
+import numpy as np
+from skimage.filters import threshold_otsu
+
+
+def dcm2png(filename):
+    # Read DICOM data
+    dcmfile = pd.dcmread(filename)
+
+    # Check if file is uint16
+    if dcmfile.PixelRepresentation != 0:
+        raise ValueError('uint16 is required')
+
+    # Read metadata
+    metadata = dcmfile.data_element
+
+    # Workflow
+    new_image = (225 * dcmfile.pixel_array / dcmfile.pixel_array.max()).astype(np.uint8)
+
+    # Convert DICOM to png file
+    new_name = filename+'.png'
+    imageio.imwrite(new_name, new_image)
+    os.remove(filename)
+
+
+def gantry_removal(img):
+    # Binarize the image using Otsu's method
+    mask = img > threshold_otsu(img)
+
+    # Remove noise
+    mask_out = np.multiply(img, mask.astype(np.uint8))
+
+    return mask_out
+
+
+def is_dicom(filepath):
+    # Try to read the file using pydicom
+    try:
+        dcm = pd.dcmread(filepath)
+        return True
+    except pd.errors.InvalidDicomError:
+        return False
