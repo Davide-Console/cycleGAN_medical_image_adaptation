d498748b68703b06d8da8275de31a87b42f03ecc
Davide-Console <davideconsole99@gmail.com>
2022-12-18 10:47:13 +0100

    testing phase updated

--- testing.py
+++ testing.py
@@ -1,28 +1,16 @@
-import csv
-import multiprocessing
-import os
+import argparse
 
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
 import torch.utils.data as torchdata
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from torch.utils.data import Subset
-from torchsummary import summary
-from tqdm import tqdm
 
 from training_utils import *
 
-path_img = 'images'
-path_b = 'batches'
-path_models = 'models'
-path_opt = 'optimizers'
+path_models = 'models/'
+path_img = 'images/'
 
-
-def main():
-    multiprocessing.freeze_support()
-
+def main(args):
     # Random seed
     torch.manual_seed(123)
 
@@ -35,126 +23,66 @@ def main():
         device = torch.device("cpu")
         print("cpu")
 
-    # splitting dataset
-    batch_size = 2
-    test_split = 0.1
+    # LOAD NETWORKS
 
-    # loading networks
-    #todo loading nets
+    G_A2B = torch.load(path_models + args.model1).to(device=device)
+    G_B2A = torch.load(path_models + args.model2).to(device=device)
+    G_A2B.eval()
+    G_B2A.eval()
 
     # DATA PREPARATION
-    dataroot = 'Data/folder1'
-    dataset_domain_A = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
+    dataroot = 'Data/folder1'  # Directory with ct img
+    dataset_A = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
         transforms.Grayscale(),
         transforms.ToTensor(),
         transforms.Normalize((0,), (1,)),
     ]))
 
-    dataset_size = len(dataset_domain_A)
-    indices = list(range(dataset_size))
+    dataset_size = len(dataset_A)
 
-    split1 = int(np.floor(test_split * dataset_size))
+    indices = list(range(dataset_size))
+    split1 = int(np.floor(args.test_split * dataset_size))
     preparation_indices, test_indices = indices[split1:], indices[:split1]
 
-    test_dataset_domain_A = Subset(dataset_domain_A, test_indices)
-    test_loader_domain_A = torchdata.DataLoader(test_dataset_domain_A, batch_size=batch_size, shuffle=True,
-                                                drop_last=True)
+    test_dataset_A = Subset(dataset_A, test_indices)
+    test_loader_A = torchdata.DataLoader(test_dataset_A, batch_size=args.batch_size, shuffle=False)
 
     dataroot = 'Data/folder2'
-    dataset_domain_B = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
+    dataset_B = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
         transforms.Grayscale(),
         transforms.ToTensor(),
         transforms.Normalize((0,), (1,)),
     ]))
 
-    test_dataset_domain_B = Subset(dataset_domain_B, test_indices)
-    test_loader_domain_B = torchdata.DataLoader(test_dataset_domain_B, batch_size=batch_size, shuffle=True,
-                                                drop_last=True)
+    test_dataset_B = Subset(dataset_B, test_indices)
+    test_loader_B = torchdata.DataLoader(test_dataset_B, batch_size=args.batch_size, shuffle=False)
 
-    # TESTING MODELS
-    n_batches_test = len(test_loader_domain_A)
+    # generate images
+    test_score_A = []
+    test_score_B = []
 
-    if save_figs:
-        os.makedirs(path_img, exist_ok=True)
-        os.makedirs(path_b, exist_ok=True)
+    n_batches_test = len(test_loader_A)
 
-    iters = 0
-    print('\nG_A2B\n', summary((G_A2B), (1, 256, 256)))
-    print('\nG_B2A\n', summary((G_B2A), (1, 256, 256)))
-    print('\nD_A\n', summary((D_A), (1, 256, 256)))
-    print('\nD_B\n', summary((D_B), (1, 256, 256)))
+    ITER1 = iter(test_loader_A)
+    ITER2 = iter(test_loader_B)
+    for i in tqdm(range(n_batches_test)):
 
-    # load data
-    validation_score_A = []
-    validation_score_B = []
-    ITER1 = iter(test_loader_domain_A)
-    ITER2 = iter(test_loader_domain_B)
-    print('Start Model Validation')
+        data_A = next(ITER1)
+        data_B = next(ITER2)
 
-    for i in tqdm(range(len(test_loader_domain_A))):
-        # for data_CTv in dataloader_test_CT:
-        data_CTv = next(ITER1)
-        data_PTv = next(ITER2)
+        A_real = data_A[0].to(device=device)
+        B_real = data_B[0].to(device=device)
 
-        A_real = data_CTv[0].to(device=device)
-        B_real = data_PTv[0].to(device=device)
-
-        # generate images
-        torch.no_grad()
-        G_A2B.eval()
-        G_B2A.eval()
         B_fake = G_A2B(A_real)
         A_fake = G_B2A(B_real)
 
-        # saving batchs if save_figs is true
-        if save_figs:
-            for k in range(len(A_real)):
-                # In the plot:
-                # 1st line: Starting image
-                # 2nd line: Generated image
-                # 3rd line: Original image
-                sample1 = B_real[k]
-                sample2 = A_fake[k]
-                sample3 = A_real[k]
-                ax1 = plt.subplot(3, len(A_real), k + 1)
-                ax1.axis('off')
-                image_viewer(sample1)
-                ax2 = plt.subplot(3, len(A_real), len(A_real) + k + 1)
-                ax2.axis('off')
-                image_viewer(sample2)
-                ax3 = plt.subplot(3, len(A_real), 2 * len(A_real) + k + 1)
-                ax3.axis('off')
-                image_viewer(sample3)
+        if args.save_figs:
+            for k in range(args.batch_size):
+                img = np.squeeze(A_fake[0].detach().numpy())
+                plt.imsave(path_img + 'pred_{}{}.png'.format(i, k), img, cmap=plt.cm.gray)
 
-            plt.savefig(path_b + '/batches_PET-CT-->CT_{}.png'.format(epoch + 1))
-
-            for k in range(len(B_real)):
-                sample1 = A_real[k]
-                sample2 = B_fake[k]
-                sample3 = B_real[k]
-                ax1 = plt.subplot(3, len(B_real), k + 1)
-                ax1.axis('off')
-                image_viewer(sample1)
-                ax2 = plt.subplot(3, len(B_real), len(B_real) + k + 1)
-                ax2.axis('off')
-                image_viewer(sample2)
-                ax3 = plt.subplot(3, len(B_real), 2 * len(B_real) + k + 1)
-                ax3.axis('off')
-                image_viewer(sample3)
-
-            plt.savefig(path_b + '/batches_CT-->PET-CT_{}.png'.format(epoch + 1))
-
-            img2 = np.squeeze(A_real[0])
-            plt.imsave(path_img + '/photoCT_r_{}.png'.format(epoch + 1), img2, cmap=plt.cm.gray)
-
-            img3 = np.squeeze(B_real[0])
-            plt.imsave(path_img + '/photoPT_r_{}.png'.format(epoch + 1), img3, cmap=plt.cm.gray)
-
-            img = np.squeeze(A_fake[0])
-            plt.imsave(path_img + '/photoCT_f_{}.png'.format(epoch + 1), img, cmap=plt.cm.gray)
-
-            img1 = np.squeeze(B_fake[0])
-            plt.imsave(path_img + '/photoPT_f_{}.png'.format(epoch + 1), img1, cmap=plt.cm.gray)
+                img1 = np.squeeze(B_fake[0].detach().numpy())
+                plt.imsave(path_img + 'pred_{}{}.png'.format(i, k), img1, cmap=plt.cm.gray)
 
         A_real = A_real.detach().cpu().numpy()
         A_fake = A_fake.detach().cpu().numpy()
@@ -163,31 +91,30 @@ def main():
 
         final_score_A, final_score_B = score(A_real, A_fake, B_real, B_fake)
 
-        validation_score_A.append(final_score_A)
-        validation_score_B.append(final_score_B)
+        test_score_A.append(final_score_A)
+        test_score_B.append(final_score_B)
 
-    score_A = np.mean(validation_score_A)
-    score_B = np.mean(validation_score_B)
-    std_A = np.std(validation_score_A)
-    std_B = np.std(validation_score_B)
+    score_A = np.mean(test_score_A)
+    score_B = np.mean(test_score_B)
+    std_A = np.std(test_score_A)
+    std_B = np.std(test_score_B)
 
-    print('CT to PET-CT Score:\tmean: ', score_A, '\tstd: ', std_A)
-    print('PET-CT to CT Score:\tmean: ', score_B, '\tstd: ', std_B)
-
-    torch.cuda.empty_cache()
-
-
-
-
-
-
-
-
-
-
+    print('A to PET-A\tmean: ', score_A, '\tstd: ', std_A)
+    print('PET-A to A\tmean: ', score_B, '\tstd: ', std_B)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        prog='ProgramName',
+        description='What the program does',
+        epilog='Text at the bottom of help')
+    parser.add_argument('-b', '--batch_size', type=int, default=8, help='batch size')
+    parser.add_argument('-m1', '--model1', type=str, default='best_G_A2B', help='first model to load')
+    parser.add_argument('-m2', '--model2', type=str, default='best_G_B2A', help='first model to load')
+    parser.add_argument('-ts', '--test_split', type=float, default=0.1,
+                        help='test/train split. Number correspont to (test set/dataset)')
+    parser.add_argument('-sf', '--save_figs', type=bool, default=False,
+                        help='If True, saves some figures for each epoch')
+    args = parser.parse_args()
 
-#todo argparse
+    main(args)
